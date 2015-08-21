067851721975ca6bdcaead15e0efcb0132aee007
Mark Kahn <m@vouchr.com>
2015-08-21 17:21:35 +0800

    Library is partially working

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-var path    = require( 'path' );
-var modules = require( 'require-all' )( path.join( __dirname, 'lib', 'prototypes' ) );
+// Configure require.paths to load local copies of owned modules instead of npm installed versions
+process.env.NODE_MODULES && require('app-module-path').addPath(process.env.NODE_MODULES);
 
-function Incoming(){}
+var path    = require('path');
+var modules = require('require-all')(path.join(__dirname, 'lib', 'prototypes'));
 
-modules.forEach( function( module ){
-  for( var proto in Object.keys( module ) ){
-    Incoming.prototype[ proto ] = module[ proto ];
-  }
-} );
+function Incoming() {
+  this.__initHandlers.forEach(function(handler) {
+    handler.call(this);
+  }, this);
+}
+Incoming.prototype.__initHandlers = [];
+
+Object.keys(modules).forEach(function(key) {
+  let module = modules[key];
+
+  Object.keys(module).forEach(function(proto) {
+    if ('init' === proto) {
+      Incoming.prototype.__initHandlers.push(module.init);
+    } else if (/^__/.test(proto)) {
+      Object.defineProperty(Incoming.prototype, proto, {
+        value : module[proto]
+      });
+    } else {
+      Incoming.prototype[proto] = module[proto];
+    }
+  });
+});
 
 module.exports = Incoming;
